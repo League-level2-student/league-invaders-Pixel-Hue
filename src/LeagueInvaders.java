04b39a66dcd06ebab952131632b5b809a5a7dcde
root <root@df325fe0bf32>
2021-11-08 00:27:48 +0000

    saDFDSA

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,18 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	static JFrame frame = new JFrame();
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	public LeagueInvaders(JFrame frame) {
+		this.frame = frame;
+	}
+public static void main(String[] args) {
+LeagueInvaders LI = new LeagueInvaders(frame);
+LI.setup();
+}
+void setup() {
+	frame.setSize(WIDTH, HEIGHT);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
 }
