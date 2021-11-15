dd3983e8d75780ac55fcc915591385a4d9557564
root <root@19db84bf982d>
2021-11-15 00:27:39 +0000

    drffgdsfgfds

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,18 +1,23 @@
 import javax.swing.JFrame;
 
 public class LeagueInvaders {
+	static GamePanel gp = new GamePanel();
 	static JFrame frame = new JFrame();
 	public static final int WIDTH = 500;
 	public static final int HEIGHT = 800;
-	public LeagueInvaders(JFrame frame) {
+	
+	public LeagueInvaders(JFrame frame, GamePanel gp) {
 		this.frame = frame;
+		this.gp = gp;
 	}
 public static void main(String[] args) {
-LeagueInvaders LI = new LeagueInvaders(frame);
+LeagueInvaders LI = new LeagueInvaders(frame, gp);
 LI.setup();
 }
 void setup() {
-	frame.setSize(WIDTH, HEIGHT);
+	frame.add(gp);
+frame.setSize(WIDTH, HEIGHT);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setVisible(true);
 }
 }
