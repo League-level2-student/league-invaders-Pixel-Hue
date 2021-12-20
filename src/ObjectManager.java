ade0e738150fb8568585551da2cfa39c1e1cac13
root <root@7e49c67a7cd6>
2021-12-20 00:26:48 +0000

    FDSAFDSA

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,7 +1,28 @@
+import java.util.ArrayList;
+import java.util.Random;
 
-public class ObjectManager {
+public class ObjectManager  {
+Rocketship rs;
+Alien alien;
 
-	public ObjectManager() {
-	
+ArrayList<Projectile> projectiles = new ArrayList();
+ArrayList<Alien> aliens = new ArrayList();
+	public ObjectManager(Rocketship rocket) {
+	this.rs=rocket;
 }
+	public void addProjectile(Projectile pj) {
+		
+	}
+	void addAlien() {
+		aliens.add(new Alien(new Random().nextInt(LeagueInvaders.WIDTH),0,50,50));
+	}
+	void update() {
+		for (int i = 0; i < aliens.size() -1; i++) {
+		aliens.get(i).update();
+		if (aliens.get(i).y > LeagueInvaders.HEIGHT) {
+			aliens.get(i).isActive = false;
+		}
+		}
+	
+	}
 }
